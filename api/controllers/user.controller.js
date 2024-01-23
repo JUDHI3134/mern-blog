fd82e3b748f197d94b584512d9c14ba7734f35fa
JUDHI3134 <judhistirbehera241@gmail.com>
2024-01-23 17:56:06 +0530

    add delete api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -43,6 +43,19 @@ export const updateUser = async (req,res,next) =>{
           res.status(200).json(rest);
         } catch (error) {
             next(error)
-        }
+        
+    }
     
+}
+
+export const deleteUser = async(req,res,next) =>{
+   if(req.user.id !== req.params.userId){
+       return next(403,'you are not allowd to delete this user')
+   }
+   try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json("User deleted successfully")
+   } catch (error) {
+    next(error)
+   }
 }
