9e44dd24dd2ecb5a684bb6552563e34ac58e56c7
JUDHI3134 <judhistirbehera241@gmail.com>
2024-01-25 00:13:23 +0530

    complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import PrivateRoute from './Components/PrivateRoute'
 import OnlyAdminPrivateRoute from './Components/OnlyAdminPrivateRoute'
 import CreatePost from './Pages/CreatePost'
 import UpdatePost from './Pages/UpdatePost'
+import PostPage from './Pages/PostPage'
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
        <Route path='/update-post/:postId' element={<UpdatePost/>}/>
        </Route>
        <Route path='/projects' element={<Project/>}/>
+       <Route path='/post/:postSlug' element={<PostPage/>}/>
      </Routes>
      <Footer/>
      </BrowserRouter>
